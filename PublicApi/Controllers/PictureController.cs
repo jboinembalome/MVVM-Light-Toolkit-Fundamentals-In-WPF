b3dac91f4be331ae700ad5e9c3cb14b8acaa585c
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-10 20:20:14 +0200

    Add Picture API

---
+++ PublicApi/Controllers/PictureController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PublicApi.Controllers
+{
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PictureController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        [Route("/api/pictures/{fileName}")]
+        public IActionResult GetPicture([FromRoute][Required] string fileName)
+        {
+            string filePath = _webHostEnvironment.ContentRootPath + $"/Content/{fileName}";
+
+            if (!System.IO.File.Exists(filePath))
+                return NotFound("File not found.");
+
+            return PhysicalFile(filePath, "image/png");
+        }
+    }
+}
