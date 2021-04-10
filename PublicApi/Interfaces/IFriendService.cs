f9a6dbd4b25bbb5c6dcf380c6b0a4ca7d56c7988
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-10 20:25:05 +0200

    Add CRUD for friend

---
+++ PublicApi/Interfaces/IFriendService.cs
@@ -0,0 +1,18 @@
+﻿using PublicApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PublicApi.Interfaces
+{
+    public interface IFriendService
+    {
+        Task<Friend> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<Friend>> ListAllAsync(CancellationToken cancellationToken = default);
+        Task<Friend> AddAsync(Friend friend, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Friend friend, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Friend friend, CancellationToken cancellationToken = default);
+    }
+}
---
+++ PublicApi/Services/FriendService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublicApi.Entities;
+using PublicApi.Infrastructure.Data;
+using PublicApi.Interfaces;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PublicApi.Services
+{
+    public class FriendService : IFriendService
+    {
+        protected readonly ContactContext _dbContext;
+
+        public FriendService(ContactContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<Friend> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var keyValues = new object[] { id };
+            return await _dbContext.Friends.FindAsync(keyValues, cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<Friend>> ListAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Friends.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Friend> AddAsync(Friend friend, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Friends.AddAsync(friend);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return friend;
+        }
+
+        public async Task UpdateAsync(Friend friend , CancellationToken cancellationToken = default)
+        {
+            _dbContext.Entry(friend).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Friend friend, CancellationToken cancellationToken = default)
+        {
+            _dbContext.Friends.Remove(friend);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
