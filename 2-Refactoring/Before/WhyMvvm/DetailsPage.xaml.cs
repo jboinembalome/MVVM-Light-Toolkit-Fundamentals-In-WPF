8aeb2bec2a9ad787bb0a79a10daa9a7810993dda
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-11 20:15:25 +0200

    Add WhyMvvm Project 2-Refactoring Before

---
+++ 2-Refactoring/Before/WhyMvvm/DetailsPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WhyMvvm
+{
+    public partial class DetailsPage : Page
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+
+            if (App.SelectedFriend != null)
+            {
+                FirstNameTextBox.Text = App.SelectedFriend.FirstName;
+                LastNameTextBox.Text = App.SelectedFriend.LastName;
+
+                PreviewFirstNameTextBlock.Text = App.SelectedFriend.FirstName;
+                PreviewLastNameTextBlock.Text = App.SelectedFriend.LastName;
+            }
+        }
+
+        private void FirstNameTextChanged(object sender, TextChangedEventArgs e)
+        {
+            App.SelectedFriend.FirstName = ((TextBox)sender).Text;
+            PreviewFirstNameTextBlock.Text = App.SelectedFriend.FirstName;
+
+        }
+
+        private void LastNameTextChanged(object sender, TextChangedEventArgs e)
+        {
+            App.SelectedFriend.LastName = ((TextBox)sender).Text;
+            PreviewLastNameTextBlock.Text = App.SelectedFriend.LastName;
+        }
+
+        private async void SaveClick(object sender, EventArgs e)
+        {
+            try
+            {
+                var service = new Model.FriendsService();
+                var result = await service.Save(App.SelectedFriend);
+
+                var id = result.Id;
+
+                if (id > 0)
+                {
+                    App.SelectedFriend.Id = id;
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+
+            base.NavigationService.GoBack();
+        }
+    }
+}
