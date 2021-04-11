08903f2b3c7d9afc83223fc5b3211b2d83d24584
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-11 20:17:09 +0200

    Add WhyMvvm.Test Project 2-Refactoring After

---
+++ 2-Refactoring/After/WhyMvvm.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WhyMvvm.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhyMvvm.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6adf819-0a7f-42c6-93f7-369ada4347f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
