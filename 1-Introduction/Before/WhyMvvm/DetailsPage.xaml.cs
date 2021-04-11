1fa27982e7a254c833c38a4a40dc26655d62c4c3
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-11 12:52:57 +0200

    Add WhyMvvm Project 1-Introduction Before

---
+++ 1-Introduction/Before/WhyMvvm/DetailsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace WhyMvvm
+{
+    public partial class DetailsPage : Window
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
