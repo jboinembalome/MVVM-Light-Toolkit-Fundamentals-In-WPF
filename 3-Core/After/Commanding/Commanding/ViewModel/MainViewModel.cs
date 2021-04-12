5c5b3b444ba0595ad75c7c36e68f2c674e1c5740
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-12 14:55:50 +0200

    Add Commanding Project 3-Core After

---
+++ 3-Core/After/Commanding/Commanding/ViewModel/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System.ComponentModel;
+
+namespace Commanding.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Counter
+        public const string CounterPropertyName = "Counter";
+
+        private int _counter;
+
+        public int Counter
+        {
+            get
+            {
+                return _counter;
+            }
+
+            set
+            {
+                if (_counter == value)
+                {
+                    return;
+                }
+
+                _counter = value;
+                RaisePropertyChanged(CounterPropertyName);
+            }
+        }
+        #endregion
+
+        public SayHello SayHelloCommand
+        {
+            get;
+            private set;
+        }
+
+        public MainViewModel()
+        {
+            SayHelloCommand = new SayHello(this);
+
+            #region Hidden
+
+            IncreaseCounterCommand = new RelayCommand(() =>
+            {
+                Counter++;
+            });
+
+            #endregion
+        }
+
+        #region INPC
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region Hidden
+
+        public RelayCommand IncreaseCounterCommand
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+    }
+}
---
+++ 3-Core/After/Commanding/Commanding/ViewModel/SayHello.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Commanding.ViewModel
+{
+    public class SayHello : ICommand
+    {
+        private readonly MainViewModel _owner;
+
+        public SayHello(MainViewModel owner)
+        {
+            _owner = owner;
+
+            _owner.PropertyChanged += (s, e) =>
+            {
+                if (CanExecuteChanged != null
+                    && e.PropertyName == MainViewModel.CounterPropertyName)
+                {
+                    CanExecuteChanged(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_owner != null
+                && _owner.Counter % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Hello, " + parameter);
+        }
+    }
+}
