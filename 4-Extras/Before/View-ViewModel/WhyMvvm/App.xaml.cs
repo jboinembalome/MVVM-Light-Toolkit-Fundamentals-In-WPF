b26a9227fd0ab4ba3c95dd39d0abb709a3b1a95f
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-13 15:23:51 +0200

    Add WhyMvvm Project 4-Extras View-ViewModel Before

---
+++ 4-Extras/Before/View-ViewModel/WhyMvvm/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WhyMvvm.Model;
+
+namespace WhyMvvm
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Friend SelectedFriend
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ 4-Extras/Before/View-ViewModel/WhyMvvm/Model/IFriendsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyMvvm.Model
+{
+    public interface IFriendsService
+    {
+        Task<IEnumerable<Friend>> Refresh();
+        Task<Friend> Save(Friend updatedFriend);
+    }
+}
