271e697176ef5dd290cd6bbf447c257957ffcf85
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-14 16:15:56 +0200

    Add EventToCommandSamples Project 4-Extras EventToCommand After

---
+++ 4-Extras/After/EventToCommand/EventToCommandSamples/Helpers/MouseButtonEventArgsToPointConverter.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EventToCommandSamples.Helpers
+{
+    public class MouseButtonEventArgsToPointConverter : IEventArgsConverter
+    {
+        public object Convert(object value, object parameter)
+        {
+            var args = (MouseButtonEventArgs)value;
+            var element = (FrameworkElement)parameter;
+
+            var point = args.GetPosition(element);
+            return point;
+        }
+    }
+}
---
+++ 4-Extras/After/EventToCommand/EventToCommandSamples/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using EventToCommandSamples.ViewModel;
+using System.Windows;
+
+namespace EventToCommandSamples
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
