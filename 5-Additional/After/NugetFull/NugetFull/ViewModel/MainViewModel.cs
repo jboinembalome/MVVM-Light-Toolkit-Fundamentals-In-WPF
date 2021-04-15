ec1b811582bf4018d11a9fc58336fbbff37ad10f
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-15 15:36:37 +0200

    Add NugetLibsOnly and NugetFull Projects 5-Additional After

---
+++ 5-Additional/After/NugetFull/NugetFull/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+using GalaSoft.MvvmLight;
+
+namespace NugetFull.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        public string Message
+        {
+            get
+            {
+                return "Hello Nuget";
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
---
+++ 5-Additional/After/NugetLibsOnly/NugetLibsOnly/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NugetLibsOnly
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                Messenger.Default.Send(new NotificationMessage("MainPageLoaded"));
+            };
+        }
+    }
+}
