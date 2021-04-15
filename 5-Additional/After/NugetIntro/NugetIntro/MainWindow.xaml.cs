a52ec0be1c9ad83dfac8ddde79fb4b6a05b03195
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-15 15:16:03 +0200

    Add NugetIntro Project 5-Additional Before and After

---
+++ 5-Additional/After/NugetIntro/NugetIntro/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Windows;
+
+namespace NugetIntro
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadJsonClick(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += ClientDownloadStringCompleted;
+            client.DownloadStringAsync(new Uri("https://localhost:44376/api/nugetintro"));
+        }
+
+        private void ClientDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null
+                && !string.IsNullOrEmpty(e.Result))
+            {
+                try
+                {
+                    var myInstance = JsonConvert.DeserializeObject<MyClass>(e.Result);
+
+                    MessageBox.Show(string.Format("String value: {0}", myInstance.Property1));
+                    MessageBox.Show(string.Format("Int value: {0}", myInstance.Property2));
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+
+    public class MyClass
+    {
+        public string Property1
+        {
+            get;
+            set;
+        }
+
+        public int Property2
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ 5-Additional/Before/NugetIntro/NugetIntro/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace NugetIntro
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadJsonClick(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += ClientDownloadStringCompleted;
+            client.DownloadStringAsync(new Uri("https://localhost:44376/api/nugetintro"));
+        }
+
+        private void ClientDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null
+                && !string.IsNullOrEmpty(e.Result))
+            {
+                try
+                {
+                    // TODO Deserialize
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+
+    public class MyClass
+    {
+        public string Property1
+        {
+            get;
+            set;
+        }
+
+        public int Property2
+        {
+            get;
+            set;
+        }
+    }
+}
