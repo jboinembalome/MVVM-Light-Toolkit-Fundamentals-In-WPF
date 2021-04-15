3dd1a8643a7e1378cd47e8704df5a3fd1f4d1b10
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-15 15:14:41 +0200

    Add NugetIntroController in PublicApi

---
+++ PublicApi/Controllers/NugetIntroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PublicApi.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    public class NugetIntroController : ControllerBase
+    {
+        /// <summary>
+        /// Get a sample json data for the NugetIntro project.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("/api/nugetintro")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public object Get()
+        {
+            return new { Property1="This is property's value", Property2=42 };
+        }
+    }
+}
