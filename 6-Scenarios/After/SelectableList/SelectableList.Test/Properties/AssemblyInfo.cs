94aa7a54ba5a5ac58763b3e8edd199c8977ac077
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-04-16 10:36:06 +0200

    Add SelectableList.Test Project 6-Scenarios After

---
+++ 6-Scenarios/After/SelectableList/SelectableList.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SelectableList.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SelectableList.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6db0d89-cf9e-48d1-b819-d4f716fbb0ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 6-Scenarios/After/SelectableList/SelectableList.Test/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SelectableList.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace SelectableList.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestExpansion()
+        {
+            Messenger.Reset();
+
+            var list = new List<SelectableViewModel>();
+
+            for (var index = 0; index < 5; index++)
+            {
+                list.Add(new SelectableViewModel());
+            }
+
+            list[0].ExpandCollapseCommand.Execute(null);
+
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item.IsExpanded);
+            }
+
+            Assert.IsTrue(list[0].IsSelected);
+        }
+
+        [TestMethod]
+        public void TestCollapse()
+        {
+            Messenger.Reset();
+
+            var list = new List<SelectableViewModel>();
+
+            for (var index = 0; index < 5; index++)
+            {
+                list.Add(new SelectableViewModel());
+            }
+
+            list[0].ExpandCollapseCommand.Execute(null);
+            list[1].IsSelected = true;
+
+            list[0].IsSelected = false;
+
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item.IsExpanded);
+            }
+
+            list[1].IsSelected = false;
+
+            foreach (var item in list)
+            {
+                Assert.IsFalse(item.IsExpanded);
+            }
+        }
+    }
+}
